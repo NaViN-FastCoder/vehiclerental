210bebaad2f35c1ca92a2b99a87fe91dbb693cce
navin <navinwadhwa63@gmail.com>
2024-02-15 16:54:59 +0530

    Finalizing Database

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 
-const Sequelizee = new Sequelize('vehiclere', 'root', 'student', {
+const Sequelizee = new Sequelize('vehicleonrent', 'root', 'student', {
     dialect: 'mysql',
     host: 'localhost'
   });
